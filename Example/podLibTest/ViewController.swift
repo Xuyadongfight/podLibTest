7ea15c3a0f1cc28c4432c151f06512855dd3b525
徐亚东 <13986885114@163.com>
2020-09-16 11:24:52 +0800

    Initial commit

---
+++ Example/podLibTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  podLibTest
+//
+//  Created by Xuyadongfight on 09/16/2020.
+//  Copyright (c) 2020 Xuyadongfight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
